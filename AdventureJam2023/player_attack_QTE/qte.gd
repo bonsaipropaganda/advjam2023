285d327a8369f3e25a5d94ff3c2452296e98d370
Ram <100201070+Yusbro@users.noreply.github.com>
2023-05-28 17:33:18 +0000

    QTE time out.
    added a tiny timeout stuff for the QTE.

--- AdventureJam2023/player_attack_QTE/qte.gd
+++ AdventureJam2023/player_attack_QTE/qte.gd
@@ -8,21 +8,31 @@ var qte_counter:int = 0
 var is_qte:bool = false
 
 
+var qte_reset_timer:float = 1.5
+var qte_timer:float = 1.5
+var qte_previous_time:float = 0
+var qte_delta_time:float = 0
+
 func _ready():
 	init_qte()
 
 
 func _process(delta):
+	qte_delta_time_process()
 	if is_qte:
 		var input:int = fetch_input()
-		qte_checker(input)
+		qte_iter(input)
+		qte_time_out()
 	else:
+		if not get_child_count()==0:
+			destroy_sprites()
 		Engine.time_scale = 1
+
+
 
 ##############################################
 func fetch_input()->int:
 	var ret_val:int = -1
-	
 	if Input.is_action_just_pressed("keyboard_up"):
 		return 0
 	if Input.is_action_just_pressed("keyboard_right"):
@@ -35,19 +45,16 @@ func fetch_input()->int:
 		return 4
 	return ret_val
 
-func qte_checker(input:int) -> void:
+
+func qte_iter(input:int) -> void:
 	if input < 0:
 		return
-	
-	
-	
 	if input == qte_sequence[qte_counter]:
+		qte_timer = qte_reset_timer
 		qte_counter += 1
 		if qte_counter == qte_sequence.size():
 			is_qte = false
-		
 		sprite_shift()
-		
 	else:
 		is_qte = false
 
@@ -55,9 +62,27 @@ func sprite_shift():
 	for i in range(get_child_count()):
 		var sprite_stuff:Object = get_child(i)
 		sprite_stuff.global_position.x -= 20
-		pass
-	pass
 
+
+
+func qte_delta_time_process()->void:
+	var current_time:float = Time.get_ticks_msec()
+	qte_delta_time = (current_time - qte_previous_time)/1000
+	qte_previous_time = current_time
+
+
+func qte_time_out()->void:
+	if qte_timer < 0:
+		qte_timer = qte_reset_timer
+		is_qte = false
+	qte_timer-=qte_delta_time
+
+
+
+func destroy_sprites()->void:
+	for i in range(get_child_count()):
+		print("CRAPPP")
+		get_child(i).queue_free()
 
 ##############################################
 func init_qte() -> void:
