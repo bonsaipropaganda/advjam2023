03b385ff2de47cd546a8b8db24bb274d291abbc8
Mathichai Dechdee <44646767+mdechdee@users.noreply.github.com>
2023-06-02 14:55:23 +0700

    format qte, zombie code

--- AdventureJam2023/player_attack_QTE/qte.gd
+++ AdventureJam2023/player_attack_QTE/qte.gd
@@ -1,68 +1,67 @@
 extends Node2D
 
+@export var images: Array[Texture]
 
-@export var images:Array[Texture]
+var called: bool = false
 
-var called:bool = false
+@export var qte_sequence: Array[int]
+@export var qte_counter: int = 0
+var is_qte: bool = false
 
+@export var qte_reset_timer: float = 1.5
+@export var qte_timer: float = 1.5
+@export var qte_previous_time: float = 0
+@export var qte_delta_time: float = 0
 
-@export var qte_sequence:Array[int]
-@export var qte_counter:int = 0
-var is_qte:bool = false
+var success: bool = false
 
+##############################################.
 
-@export var qte_reset_timer:float = 1.5
-@export var qte_timer:float = 1.5
-@export var qte_previous_time:float = 0
-@export var qte_delta_time:float = 0
-
-var success:bool = false
-
-
-
-##############################################. 
 
 #this is where the whole code starts
-func init_qte(sequenceLength : int = 3,resetBonusTime : float = 1.5,totalCompletionTime : float = 3.0) -> void:
+func init_qte(
+	sequenceLength: int = 3, resetBonusTime: float = 1.5, totalCompletionTime: float = 3.0
+) -> void:
 	is_qte = true
-	
+
 	qte_reset_timer = resetBonusTime
 	qte_timer = totalCompletionTime
-	
+
 	#get a random sequence of stuff
 	qte_sequence = sequence(sequenceLength)
-	
+
 	#spawn some sprites
 	spawn_qte_sprites()
-	
+
 	#stop the time
 	Engine.time_scale = 0.0
-	
+
 	#hey, da QTE timer!!!. AHHH!!!!, TODO: make some sorta weird shader that'll slowly make the screen go grayscale to indicate that the time's gonna run out
 	qte_timer = qte_reset_timer
-	qte_counter = 0#how many correct characters the player has ah. done.
+	qte_counter = 0  #how many correct characters the player has ah. done.
+
 
 #get the random sequence of characetsrs
-func sequence(max_len:int)->Array[int]:
-	@warning_ignore("unassigned_variable")
-	var seq:Array[int]
+func sequence(max_len: int) -> Array[int]:
+	var seq: Array[int] = []
 	for i in range(max_len):
-		var value:int = randi_range(0, images.size() - 2)
+		var value: int = randi_range(0, images.size() - 2)
 		seq.append(value)
 	return seq
-	
+
+
 #spawn the sprites
 func spawn_qte_sprites():
-	var counter:int = 0
-	var player:Object = get_parent()
-	
+	var counter: int = 0
+	var player: Object = get_parent()
+
 	for i in qte_sequence:
-		var sprite:Sprite2D = Sprite2D.new()
+		var sprite: Sprite2D = Sprite2D.new()
 		add_child(sprite)
-		
+
 		sprite.texture = images[i]
-		sprite.global_position =Vector2(counter*20, 0) + player.global_position
-		
+		sprite.global_position = Vector2(counter * 20, 0) + player.global_position
+
 		counter += 1
 
 
@@ -70,25 +69,28 @@ func spawn_qte_sprites():
 func _process(_delta):
 	#custom delta time, cuz time has been stopped now. soo no traditional delta time
 	qte_delta_time_process()
-	if is_qte:#are you still playing da QTE?.
-		var input:int = fetch_input()#get da input
-		qte_iter(input)#see if it's right. if it is, check iterate
-		qte_time_out()#timed out?. exit da QTE
-	if is_qte==false:#if the qte is done
-		if not get_child_count()==0:#delete all the sprites
+	if is_qte:  #are you still playing da QTE?.
+		var input: int = fetch_input()  #get da input
+		qte_iter(input)  #see if it's right. if it is, check iterate
+		qte_time_out()  #timed out?. exit da QTE
+	if is_qte == false:  #if the qte is done
+		if not get_child_count() == 0:  #delete all the sprites
 			destroy_sprites()
-		Engine.time_scale = 1#set the engine time to 1.
+		Engine.time_scale = 1  #set the engine time to 1.
+
 
 #export functions!!!
-func is_success()->bool:
+func is_success() -> bool:
 	return success
 
-func reset()->void:
+
+func reset() -> void:
 	success = false
 
+
 ##############################################
-func fetch_input()->int:
-	var ret_val:int = -1
+func fetch_input() -> int:
+	var ret_val: int = -1
 	if Input.is_action_just_pressed("keyboard_up"):
 		return 0
 	if Input.is_action_just_pressed("keyboard_right"):
@@ -100,44 +102,46 @@ func fetch_input()->int:
 	return ret_val
 
 
-func qte_iter(input:int) -> void:
-	if input < 0:#if there has NOT been any input, exit 
+func qte_iter(input: int) -> void:
+	if input < 0:  #if there has NOT been any input, exit
 		return
-	
+
 	#yayyy!!, if the input is right, resert the qte timer. and iterate the qte_counter so you can check the next element in the sequence
 	if input == qte_sequence[qte_counter]:
 		if qte_reset_timer > 0:
-			qte_timer = clampf(qte_timer+qte_reset_timer,0,qte_timer)
+			qte_timer = clampf(qte_timer + qte_reset_timer, 0, qte_timer)
 		qte_counter += 1
-		if qte_counter == qte_sequence.size():#if you'er at the end of the sequence, exitttt, and sucess is truee
+		if qte_counter == qte_sequence.size():  #if you'er at the end of the sequence, exitttt, and sucess is truee
 			is_qte = false
 			success = true
 		sprite_shift()
-	else:# if you have failed, then just quit
+	else:  # if you have failed, then just quit
 		is_qte = false
 
-func sprite_shift():#for moving the sprite around after every input
+
+func sprite_shift():  #for moving the sprite around after every input
 	for i in range(get_child_count()):
-		var sprite_stuff:Object = get_child(i)
+		var sprite_stuff: Object = get_child(i)
 		sprite_stuff.position.x -= 20
 
 
-func qte_delta_time_process()->void:#delta timeee,when the time is stopped
-	var current_time:float = Time.get_ticks_msec()
-	qte_delta_time = (current_time - qte_previous_time)/1000
+func qte_delta_time_process() -> void:  #delta timeee,when the time is stopped
+	var current_time: float = Time.get_ticks_msec()
+	qte_delta_time = (current_time - qte_previous_time) / 1000
 	qte_previous_time = current_time
-	
+
 	for i in range(get_child_count()):
-		get_child(i).scale = Vector2(1,1) * (qte_timer/(qte_timer-qte_delta_time))
+		get_child(i).scale = Vector2(1, 1) * (qte_timer / (qte_timer - qte_delta_time))
 
 
-func qte_time_out()->void:#timedd out
+func qte_time_out() -> void:  #timedd out
 	if qte_timer < 0:
 		qte_timer = qte_reset_timer
 		is_qte = false
-	qte_timer-=qte_delta_time
+	qte_timer -= qte_delta_time
 
-func destroy_sprites()->void:
+
+func destroy_sprites() -> void:
 	for i in range(get_child_count()):
 		get_child(i).queue_free()
 	Engine.time_scale = 1
--- AdventureJam2023/scripts/Entities/zombie.gd
+++ AdventureJam2023/scripts/Entities/zombie.gd
@@ -2,55 +2,57 @@ extends CharacterBody2D
 
 class_name Zombie
 
-@onready var player:Object = get_tree().get_nodes_in_group("Player")[0]
-
+@onready var player: Object = get_tree().get_nodes_in_group("Player")[0]
 
 @onready var alive = true
 
-var speed:float = 400
-var direction:Vector2 = Vector2(-0.5,0.0)
+var speed: float = 400
+var direction: Vector2 = Vector2(-0.5, 0.0)
 
 #wander stuff
-var wander_timer:float = 3
+var wander_timer: float = 3
 
 #chase stuff
-var chase_distance:float = 80
-var is_chasing:bool = false
+var chase_distance: float = 80
+var is_chasing: bool = false
+
 
 func _process(delta):
 	if alive:
 		move(delta)
 		walk_animation()
 
-func move(delta:float)->void:
-	var distance_from_player:float = player.global_position.distance_to(global_position)
-	
+
+func move(delta: float) -> void:
+	var distance_from_player: float = player.global_position.distance_to(global_position)
+
 	if distance_from_player > chase_distance:
 		direction = wander(delta)
 	if distance_from_player < chase_distance or is_chasing:
 		is_chasing = true
 		speed = 700
 		direction = chase()
-	
+
 	velocity = direction * speed * delta
 	move_and_slide()
 
 
-func wander(delta:float)->Vector2:
+func wander(delta: float) -> Vector2:
 	if wander_timer < 0:
-		var random_angle:float = randf_range(0, 2*PI)
-		wander_timer = randf_range(1,2)
+		var random_angle: float = randf_range(0, 2 * PI)
+		wander_timer = randf_range(1, 2)
 		return Vector2(cos(random_angle), sin(random_angle))
 	wander_timer -= delta
 	return direction
 
-func chase()->Vector2:
+
+func chase() -> Vector2:
 	return (player.global_position - global_position).normalized()
 
 
-func walk_animation()->void:
+func walk_animation() -> void:
 	$AnimatedSprite2D.flip_h = velocity.x < 0
-	if abs(direction.y) >= abs(direction.x): 
+	if abs(direction.y) >= abs(direction.x):
 		if direction.y < 0:
 			$AnimatedSprite2D.set_animation("walk_up")
 		else:
@@ -59,30 +61,23 @@ func walk_animation()->void:
 		$AnimatedSprite2D.set_animation("walk_side")
 
 
-
 #for all the death stuff!!!
 func _on_area_2d_area_entered(area):
 	if "SwordSlash" in area.name and alive:
+		print("OUCH")
 		alive = false
 		is_chasing = false
 		match $AnimatedSprite2D.animation:
-			"idle_down":
+			"idle_down", "walk_down":
 				$AnimatedSprite2D.set_animation("death_down")
-			"walk_down":
-				$AnimatedSprite2D.set_animation("death_down")
-			
-			"idle_up":
+
+			"idle_up", "walk_up":
 				$AnimatedSprite2D.set_animation("death_up")
-			"walk_up":
-				$AnimatedSprite2D.set_animation("death_up")
-			
-			"idle_side":
-				$AnimatedSprite2D.set_animation("death_side")
-			"walk_side":
+
+			"idle_side", "walk_side":
 				$AnimatedSprite2D.set_animation("death_side")
 		collision_layer = 0
 		set_process(false)
-
 
 #
 #func _on_animated_sprite_2d_animation_finished():
