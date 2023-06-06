fd0aa647908dc9f6cd2874c9c54f513275555396
zomdiax5 <45743145+zomdiax5@users.noreply.github.com>
2023-06-06 10:00:25 +0200

    Volume controls + setup busses

--- AdventureJam2023/scripts/GUI/menus/main_menu.gd
+++ AdventureJam2023/scripts/GUI/menus/main_menu.gd
@@ -1,4 +1,9 @@
 extends Node2D
+@onready var test_sound = $Control/SoundVolume/TestSound
+
+func _ready(): # Setup default sound values
+	_on_music_volume_value_changed($Control/MusicVolume.value)
+	_on_sound_volume_value_changed($Control/SoundVolume.value)
 
 func _on_texture_button_button_down():#Play
 	get_tree().change_scene_to_file("res://scenes/main.tscn")
@@ -8,3 +13,12 @@ func _on_texture_button_3_button_down():#credits
 
 func _on_texture_button_2_button_down():# quit
 	get_tree().quit()
+
+
+func _on_music_volume_value_changed(value):
+	AudioServer.set_bus_volume_db(AudioServer.get_bus_index("Music"), linear_to_db(value))
+
+
+func _on_sound_volume_value_changed(value):
+	AudioServer.set_bus_volume_db(AudioServer.get_bus_index("Sounds"), linear_to_db(value))
+	test_sound.play()
