126ab888f8e02bb2becb25e4d682f70195c199e4
Khusheete <Siandfrance@gmail.com>
2023-06-08 00:54:39 +0200

    Fixed DarkFigure exit sequence

--- AdventureJam2023/scripts/Entities/NPCs/DarkFigure.gd
+++ AdventureJam2023/scripts/Entities/NPCs/DarkFigure.gd
@@ -1,16 +1,14 @@
 extends NPC
 
 
-func _action(act_name: String) -> void:
-	match act_name:
-		"open_path_and_disappear":
-			key.visible = false
-			text_box.visible = false
-			$CollisionShape2D.disabled = true
-			SpriteAnimator.play("disappear")
-			get_parent().get_node("ForceField").get_node("AnimatedSprite2D").play("disappear")
-			$AudioStreamPlayer2D.play(0)
-			await SpriteAnimator.animation_finished
-			get_parent().get_node("ForceField").queue_free()
-			await $AudioStreamPlayer2D.finished
-			queue_free() # FIXME: idk why, this is never called
+func _on_dialogue_finished(dialogue_name: String) -> void:
+	if dialogue_name == "Meeting":
+		key.visible = false
+		$InteractionRange.monitoring = false
+		SpriteAnimator.play("disappear")
+		get_parent().get_node("ForceField").get_node("AnimatedSprite2D").play("disappear")
+		$AudioStreamPlayer2D.play(0)
+		await SpriteAnimator.animation_finished
+		get_parent().get_node("ForceField").queue_free()
+		$AudioStreamPlayer2D.stop()
+		queue_free() # TODO ?
