cbc5b4bb907938f8e3b637b9d686dbdd3fec741d
Khusheete <Siandfrance@gmail.com>
2023-06-07 16:26:20 +0200

    Changed the way the boat works to fix camera on enter

--- AdventureJam2023/scenes/Entities/Boat.gd
+++ AdventureJam2023/scenes/Entities/Boat.gd
@@ -7,6 +7,7 @@ var timer :float = 0
 @onready var key = $Boat/InteractionRange/Key
 @onready var animation_player = $AnimationPlayer
 @onready var color_rect = $Boat/ColorRect
+@onready var remote_transform: RemoteTransform2D = $Boat/RemoteTransform2D
 
 var flipped :bool = false
 var is_travelling :bool = false
@@ -15,19 +16,8 @@ var can_interact :bool = false
 
 func _physics_process(delta):
 	if can_interact and Input.is_action_just_pressed("Interact") and not is_travelling:
-		is_travelling = true
-		if flipped:
-			animation_player.play_backwards("new_animation")
-		else:
-			animation_player.play("new_animation")
-		flipped = !flipped
-		player.get_node("HitBox").disabled = true
-		player.get_parent().remove_child(player)
-		boat_sprite.add_child(player)
-		player.scale = Vector2(0.3,0.3)
-		player.global_position = target.global_position
-		player.show_behind_parent = true
-		player.paused = true
+		start_travel()
+
 
 func _process(delta):	# Animates the boat movement using sin waves
 	timer += delta/2
@@ -35,18 +25,18 @@ func _process(delta):	# Animates the boat movement using sin waves
 		timer+=delta # Speedup animation a little while traveling
 	boat_sprite.rotation_degrees = 0 + (sin(timer)*5)
 	boat_sprite.position.y = 0 + (sin(timer*2)*3)
-	if is_travelling:
-		player.global_position = lerp(player.global_position,target.global_position,5*delta)
 	
 	key.visible = can_interact
 	if flipped:
 		key.scale.x = -1
 	else:
 		key.scale.x = 1
-	
+
+
 func _on_interaction_range_body_entered(body):
 	if body.name == "Player":
 		can_interact = true
+
 
 # this is to exit the interaction
 func _on_interaction_range_body_exited(body):
@@ -54,13 +44,27 @@ func _on_interaction_range_body_exited(body):
 		can_interact = false
 
 
+func start_travel() -> void:
+	is_travelling = true
+	if flipped:
+		animation_player.play_backwards("new_animation")
+	else:
+		animation_player.play("new_animation")
+	flipped = !flipped
+	player.get_node("HitBox").disabled = true
+	remote_transform.remote_path = player.get_path()
+	player.global_position = target.global_position
+	player.show_behind_parent = true
+	player.paused = true
+
+
+
 # HORRIBLE way to move the player and return him
+# Edit: I think I made it a bit better !
 func _on_animation_player_animation_finished(anim_name):
-	boat_sprite.remove_child(player)
-	get_parent().get_parent().add_child(player)
+	remote_transform.remote_path = ""
 	player.global_position = target.global_position
 	is_travelling = false
 	player.get_node("HitBox").disabled = false
 	player.show_behind_parent = false
-	player.scale = Vector2(1,1)
 	player.paused = false
